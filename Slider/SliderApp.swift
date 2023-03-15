05351411f248cc83f63fa675edbca7cf8655788d
Valentina <balendi.art@gmail.com>
2023-03-15 12:11:32 +0100

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Apprenant 18 on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
